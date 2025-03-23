1f26417dcc3e2b2974d8f3d61611c5482f49d41c
D Srinivas Karthik <dsrinivaskarthik@gmail.com>
2025-03-23 22:11:58 +0530

    Initial Spring Boot project setup
    Added the following dependencies through spring-initializr
    - Spring Web: For REST APIs
    - Spring Boot DevTools: Hot reload
    - Lombok: To reduce boilerplate code
    - Spring Data JPA: For database interactions
    - PostgreSQL Driver

---
+++ src/main/java/com/firstup/FirstupApplication.java
@@ -0,0 +1,13 @@
+package com.firstup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstup/FirstupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
